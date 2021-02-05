f17497604774baa2140604ebc6721fc20e5a9827
Balázs Molnár <molbazsa@gmail.com>
2021-02-05 10:45:23 +0100

    using state

--- src/Products.js
+++ src/Products.js
@@ -1,11 +1,27 @@
+import { useEffect, useState } from "react";
+
 function Products() {
   const title = "Üdv a rendszerben!";
-  const products = 50;
+  const [products, setProducts] = useState(50);
+
+  useEffect(() => {
+    console.log(`products = ${products} now`);
+  }, [products]);
+
+  function handleMinus() {
+    setProducts(products - 1);
+  }
+
+  function handlePlus() {
+    setProducts(products + 1);
+  }
 
   return (
     <div className="Products">
       <h1>{title}</h1>
       <p>Jelenleg {products} terméket tartok számon</p>
+      <button onClick={handleMinus}>-</button>
+      <button onClick={handlePlus}>+</button>
     </div>
   );
 }
