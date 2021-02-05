f8c14e82aeba27012219fdee1e7639e64c70883a
Balázs Molnár <molbazsa@gmail.com>
2021-02-05 16:31:42 +0100

    fetch from json server

--- src/Home.js
+++ src/Home.js
@@ -1,26 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ProductList from "./ProductList";
 
 function Home() {
-  const [products, setProducts] = useState([
-    {
-      id: 1,
-      barcode: 25638425234,
-      name: "Snickers",
-      description: "csokoládé - 100g",
-    },
-    { id: 2, barcode: 76787256725, name: "Tomi", description: "mosószer - 1L" },
-    {
-      id: 3,
-      barcode: 65432735432,
-      name: "Coca-Cola",
-      description: "üdítő - 1,25L",
-    },
-  ]);
+  const [products, setProducts] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/products")
+      .then((response) => response.json())
+      .then((data) => setProducts(data));
+  }, []);
 
   return (
     <div className="Home">
-      <ProductList title="Minden termék" products={products} />
+      {products && <ProductList title="Minden termék" products={products} />}
     </div>
   );
 }
